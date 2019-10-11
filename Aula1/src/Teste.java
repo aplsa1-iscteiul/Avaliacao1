9f2c443e02001fe93351be8c7ed6e9a197d9b6c8
Andre Lopes <andreplopes99@hotmail.com>
2019-10-11 10:55:04 +0100

    Classe teste criada

---
+++ Aula1/src/Teste.java
@@ -0,0 +1,21 @@
+
+public class Teste {
+	
+	private String cadeira;
+	private int valores;
+	private boolean aprovado;
+	private static final int  notaMinima = 10;
+	
+	public Teste(String cadeira, int valores){
+		this.cadeira = cadeira;
+		this.valores = valores;
+	}
+	
+	public void resultado(){
+		if (this.valores >= notaMinima)
+			aprovado = true;
+		else 
+			aprovado = false;
+	}
+
+}
